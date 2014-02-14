21a03b94c22365f22b74458f0e3bc1491c341469
Pedram Hadjian <pedram.hadjian@gmail.com>
2014-02-14 13:12:52 +0100

    CMakeLists.txt: - CMake convention seems to be upper-case for variables and lower-case for   functions. Adjusted the project's CMakeLists.txt appropriately. - The build type, which is defined by the "BUILD_TYPE" variable, defaults   to "Release". This default can be modified by the command-line argument to   cmake: cmake -DCMAKE_BUILD_TYPE:STRING={Debug|Release} . - Workaround for the external repo deletion problem when rerunning cmake (closes   #2). See the ticket for details on the solution. - Moved cmake-related files to a cmake/ folder to unclutter the project dir. - All external project's BINARY_DIRs point to build/<project-name>-<build-type>,   such that each build-type has its own subdirectory outside the src-tree.
    cmake/safe_git_clone.cmake:
    - CMake script to check for existing repos before cloning them.
    - Using cmake makes this script platform-independent (I hope).
    
    .gitignore:
    - Generated directory "build" will be ignored by git.

---
+++ cmake/safe_git_clone.cmake
@@ -0,0 +1,19 @@
+#------------------------------------------------------------------------------
+# This script clones the repository/tag to the CLONE_DIR specified by the following
+# variables:
+# 
+#   CLONE_URL  - Url to git rep (e.g. git@github.com:<user>/<repo>.git).
+#   CLONE_TAG  - Branch of the git repo to check out.
+#   CLONE_DIR  - Name of directory to which to clone to.
+#   
+
+if(DEFINED CLONE_URL AND DEFINED CLONE_TAG AND DEFINED CLONE_DIR)
+  if(NOT EXISTS ${CLONE_DIR}/.git)
+    message("Cloning ${CLONE_URL}!")
+    execute_process(COMMAND git clone --branch ${CLONE_TAG} ${CLONE_URL} ${CLONE_DIR})     
+  else(NOT EXISTS ${CLONE_DIR}/.git)
+    message("Path ${CLONE_DIR}/.git already exists. Skipping clone.")
+  endif(NOT EXISTS ${CLONE_DIR}/.git)
+else(DEFINED CLONE_URL AND DEFINED CLONE_TAG AND DEFINED CLONE_DIR)
+  message("Did not clone anything. One of CLONE_URL, CLONE_TAG or CLONE_DIR was not defined.")
+endif(DEFINED CLONE_URL AND DEFINED CLONE_TAG AND DEFINED CLONE_DIR)
